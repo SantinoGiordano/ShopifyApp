50aabd2134035c9aa425d387b6b989d8d53d7c75
SantinoGiordano <santinogiordano13@gmail.com>
2025-07-22 15:31:48 -0400

    fixfed the page of the checkout

--- app/(root)/checkout/page.tsx
+++ app/(root)/checkout/page.tsx
@@ -5,24 +5,20 @@ import { useEffect } from "react";
 import convertToSubcurrency from "@/lib/convertToSubcurrency";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-
 import { useCartStore } from "@/store/cartStore";
 import CheckoutPage from "@/app/components/CheckoutPage";
 
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
-);
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
 export default function Checkout() {
   const totalPrice = useCartStore((state) => state.totalPrice);
-  const calculateTotalPrice = useCartStore(
-    (state) => state.calculateTotalPrice
-  );
+  const calculateTotalPrice = useCartStore((state) => state.calculateTotalPrice);
   const cart = useCartStore((state) => state.cart);
 
-useEffect(() => {
-  calculateTotalPrice(cart);
-}, [cart]);
+  useEffect(() => {
+    calculateTotalPrice(cart);
+  }, [calculateTotalPrice, cart]);
 
   const isValidAmount = totalPrice > 0;
 
@@ -39,12 +35,10 @@ useEffect(() => {
               currency: "usd",
             }}
           >
-            <CheckoutPage totalPrice={totalPrice} />
+            <CheckoutPage cart={cart} totalPrice={totalPrice} />
           </Elements>
         ) : (
-          <p className="text-center text-red-500">
-            Add items to cart to checkout
-          </p>
+          <p className="text-center text-red-500">Add items to cart to checkout</p>
         )}
       </div>
     </main>
